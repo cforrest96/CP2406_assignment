b5938cfde71b816f016410b59ffd79fb2e975dd7
caleb.forrest@my.jcu.edu.au <caleb2626>
2019-09-07 16:21:43 +1000

    add minor tweaks to classes

--- src/Car.java
+++ src/Car.java
@@ -3,40 +3,40 @@ public class Car {
 
     private double vehicle_length;
     private int car_position;
-    public int car_road;
+    private int car_road;
 
-    public Car() {
+    Car() {
         vehicle_length = 0.5;
         car_position = 1;
         car_road = 1;
     }
-    public Car(double vehicle_length, int car_position, int car_road) {
+    Car(double vehicle_length, int car_position, int car_road) {
         set_vehicle_length(vehicle_length);
         set_car_position(car_position);
         set_car_road(car_road);
     }
 
-    public double get_vehicle_length() {
+    double get_vehicle_length() {
         return vehicle_length;
     }
 
-    public void set_vehicle_length(double vehicle_length) {
+    void set_vehicle_length(double vehicle_length) {
         this.vehicle_length = vehicle_length;
     }
 
-    public int get_car_position() {
+    int get_car_position() {
         return car_position;
     }
 
-    public void set_car_position(int car_position) {
+    void set_car_position(int car_position) {
         this.car_position = car_position;
     }
 
-    public int get_car_road() {
+    int get_car_road() {
         return car_road;
     }
 
-    public void set_car_road(int car_road) {
+    void set_car_road(int car_road) {
         this.car_road = car_road;
     }
 
--- src/Road.java
+++ src/Road.java
@@ -4,47 +4,47 @@ public class Road {
     private String road_shape;
     private Boolean road_light;
 
-    public Road(){
+    Road(){
         road_length = 4;
         road_num = 1;
         road_light = true;
     }
 
-    public Road(int road_length, int road_num, Boolean road_light){
+    Road(int road_length, int road_num, Boolean road_light){
         set_road_num(road_num);
         set_road_length(road_length);
         set_road_light(false);
     }
 
-    public int get_road_length() {
+    int get_road_length() {
         return road_length;
     }
 
-    public void set_road_length(int road_length) {
+    void set_road_length(int road_length) {
         this.road_length = road_length;
     }
 
-    public int get_road_num() {
+    int get_road_num() {
         return road_num;
     }
 
-    public void set_road_num(int road_num) {
+    void set_road_num(int road_num) {
         this.road_num = road_num;
     }
 
-    public String get_road_shape() {
+    String get_road_shape() {
         return road_shape;
     }
 
-    public void set_road_shape(String road_shape) {
+    void set_road_shape(String road_shape) {
         this.road_shape = road_shape;
     }
 
-    public Boolean get_road_light() {
+    Boolean get_road_light() {
         return road_light;
     }
 
-    public void set_road_light(Boolean road_light) {
+    void set_road_light(Boolean road_light) {
         this.road_light = road_light;
     }
 }
